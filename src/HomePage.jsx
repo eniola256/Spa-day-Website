2502c1ca09f7da19a0bc870edf5425661f39e2e8
Eniola <eniolaa177@gmail.com>
2025-10-23 03:37:16 +0100

    Initial commit

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -10,28 +10,9 @@ import { useEffect } from "react";
 
 function HomePage() {
   
-     useEffect(() => {
-    const targets = document.querySelectorAll(".all-msg");
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("visible");
-          }
-        });
-      },
-      { threshold: 0.3 }
-    );
-
-    targets.forEach((el) => observer.observe(el));
-
-    return () => {
-      targets.forEach((el) => observer.unobserve(el));
-    };
-  }, []); // <-- ✅ properly closed useEffect
-
+  
   return (
+    
     <>
       <div className="Body">
         {/* ====== MAIN HERO ====== */}
@@ -47,43 +28,41 @@ function HomePage() {
         {/* ====== WELCOME MESSAGE ====== */}
         <div className="welcome-msg">
           <div className="all-msg">
-            <div className="welcome-msg-txt slde-bottom">
-              <h3>Welcome to your luxurious home away from home</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at
-                consequatur dolorum excepturi, reprehenderit, sit perspiciatis
-                quibusdam aspernatur accusamus distinctio repellendus enim
-                asperiores magni eum dolores doloribus non autem totam sequi
-                nesciunt error voluptatum adipisci ipsa. Beatae in voluptatibus
-                nulla ad provident nostrum incidunt consectetur!
-              </p>
-              <Link to="/about">
-                <button className="message-btn">More about us</button>
-              </Link>
-            </div>
-            <div className="welcome-msg-img">
-              <img src="./welcome.jpg" alt="Welcome" />
-            </div>
+          <div className="welcome-msg-txt slde-bottom">
+            <h3>Welcome to your luxurious home away from home</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at
+              consequatur dolorum excepturi, reprehenderit, sit perspiciatis
+              quibusdam aspernatur accusamus distinctio repellendus enim
+              asperiores magni eum dolores doloribus non autem totam sequi
+              nesciunt error voluptatum adipisci ipsa. Beatae in voluptatibus
+              nulla ad provident nostrum incidunt consectetur!
+            </p>
+            <Link to="/about"><button className="message-btn">More about us</button></Link>
+          </div>
+          <div className="welcome-msg-img">
+            <img src="./welcome.jpg" alt="Welcome" />
+          </div>
           </div>
         </div>
 
         {/* ====== WELCOME MESSAGE (MOBILE VERSION) ====== */}
         <div className="welcome-msg2">
           <div className="all-msg">
-            <div className="welcome-msg-txt">
-              <h3>Welcome to your luxurious home away from home</h3>
-              <div className="welcome-msg-img">
-                <img src="./welcome.jpg" alt="Welcome" />
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at
-                consequatur dolorum excepturi, reprehenderit, sit perspiciatis
-                quibusdam aspernatur accusamus distinctio repellendus enim
-                asperiores magni eum dolores doloribus non autem totam sequi
-                nesciunt error voluptatum adipisci ipsa. Beatae in voluptatibus
-                nulla ad provident nostrum incidunt consectetur!
-              </p>
+          <div className="welcome-msg-txt">
+            <h3>Welcome to your luxurious home away from home</h3>
+            <div className="welcome-msg-img">
+              <img src="./welcome.jpg" alt="Welcome" />
             </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at
+              consequatur dolorum excepturi, reprehenderit, sit perspiciatis
+              quibusdam aspernatur accusamus distinctio repellendus enim
+              asperiores magni eum dolores doloribus non autem totam sequi
+              nesciunt error voluptatum adipisci ipsa. Beatae in voluptatibus
+              nulla ad provident nostrum incidunt consectetur!
+            </p>
+          </div>
           </div>
         </div>
 
@@ -93,7 +72,7 @@ function HomePage() {
           <div className="rooms">
             {/* Room 1 */}
             <div className="room r1">
-              <img src="./room1.jpg" alt="room1" />
+                <img src="./room1.jpg" alt="room1" />
               <div className="room-txt">
                 <div className="room-up-txt">
                   <p>2 adults | 1 child below 7</p>
@@ -133,21 +112,14 @@ function HomePage() {
               </div>
             </div>
           </div>
-
           <div className="extra">
             <div className="extra-left acc">
-              <img src="./Sauna.jpg" alt="" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Nesciunt, molestias! Pariatur
-              </p>
+                <img src="./Sauna.jpg" alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, molestias! Pariatur </p>
             </div>
             <div className="extra-right acc">
-              <img src="./pool.jpg" alt="" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Nesciunt, molestias! Pariatur repudiandae quod hic facilis!
-              </p>
+                <img src="./pool.jpg" alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, molestias! Pariatur repudiandae quod hic facilis!</p>
             </div>
           </div>
         </div>
@@ -200,4 +172,5 @@ function HomePage() {
     </>
   );
 }
+
 export default HomePage
